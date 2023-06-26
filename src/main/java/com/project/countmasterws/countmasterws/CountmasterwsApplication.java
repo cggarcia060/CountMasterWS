07d297bed848f58cb1d67196a50c13954954a78f
cgarcia <105094615+cggarcia060@users.noreply.github.com>
2023-06-26 11:46:42 -0500

    Start project

---
+++ src/main/java/com/project/countmasterws/countmasterws/CountmasterwsApplication.java
@@ -0,0 +1,13 @@
+package com.project.countmasterws.countmasterws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountmasterwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountmasterwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/countmasterws/countmasterws/CountmasterwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.countmasterws.countmasterws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountmasterwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
